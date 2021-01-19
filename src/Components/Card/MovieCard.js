bc0d14ae5c2a99fb09323f7eb14deb58895a2e3c
lucasdipaula <lucasdepaula@dcc.ufmg.br>
2021-01-19 18:18:54 -0300

    aplicando nova estilizaçao

--- src/Components/Card/MovieCard.js
+++ src/Components/Card/MovieCard.js
@@ -9,7 +9,7 @@ const MovieCard = (props) => {
 
   const renderNote = (props) => {
     return (
-      <div className='movie-card__note'>
+      <div className={classes.movieCardNote}>
         <span>{props.note}</span>
       </div>
     )
@@ -17,25 +17,25 @@ const MovieCard = (props) => {
   
   const renderViews = (props) => {
     return (
-      <div className='movie-card__views'>
-        <span className='movie-card__views__text'>{props.views}</span>
+      <div className={classes.movieCardViews}>
+        <span className={classes.movieCardViewsText}>{props.views}</span>
       </div>
     )
   }
   
   const renderMovieText = (props) => {
     return (
-    <span className='movie-card__text'>{props.title}</span>
+    <span className={classes.movieCardText}>{props.title}</span>
     )
   }
   
 
   return (
-    <div className="movie-card_size">
-      <div className="movie-card">
+    <div className={classes.movieCardSize}>
+      <div className={classes.MovieCard}>
         {renderNote(props)}
         <img
-          className='movie-card__image'
+          className={classes.movieCardImage}
           src={props.image}
           alt='Cartaz do filme minha mae é uma peça'
         />
