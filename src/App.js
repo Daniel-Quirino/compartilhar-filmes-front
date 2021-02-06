89045217c34763d6d22608fe1b4b695ed956f784
Daniel Quirino <danielpires6@hotmail.com>
2021-02-06 17:57:07 -0300

    feature: Search Bar para buscar por fimes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core';
 
 import Header from './Components/Header/Header';
 import Home from './Pages/Home/Home';
-
+import SearchMovie from './Pages/SearchMovie';
 
 function App() {
+  const [ searchMovie, setSearchMovie ] = useState('');
+  const getMovie = (movie) => {
+    setSearchMovie(movie)
+  }
 
   const theme = createMuiTheme({
     palette: {
@@ -19,10 +23,11 @@ function App() {
     },
   });
 
+
   return (
     <ThemeProvider theme={theme}>
-      <Header/>
-      <Home/>
+      <Header getMovie={getMovie}/>
+      {searchMovie ? <SearchMovie searchMovie={searchMovie}/> : <Home/>}
     </ThemeProvider>
   )
 }
