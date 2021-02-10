e8dd48da3fc071477af94a901a24966c944558ad
Daniel Quirino <danielpires6@hotmail.com>
2021-02-10 16:50:27 -0300

    Update Button.js

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const DefaultButton = (props) => {
   const classes = useStyles();
 
+  
   return (
       <Button 
         variant="contained"
