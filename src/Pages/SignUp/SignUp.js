3bac2bf679226c32906a50405d1918fed1636822
lucasdepaula@dcc.ufmg.br <lucasdepaula@dcc.ufmg.br>
2021-02-09 23:24:36 -0300

    integrando cadastro de usuario na api

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -8,13 +8,44 @@ import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
-import movie from '../../assets/movie.png'
+import { useHistory } from "react-router-dom";
 
+import movie from '../../assets/movie.png';
+import createUser from '../../service/users/index';
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
 
 const SignUp = (props) => {
   
   const classes = useStyles();
+  const history = useHistory();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [city, setCity] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const [open, setOpen] = useState(false);
+  const [severity, setSeverity] = useState('');
+  const [notificationMessage, setNotificationMessage] = useState('');
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
 
   const Copyright = () => {
       return (
@@ -27,6 +58,50 @@ const SignUp = (props) => {
             {'.'}
           </Typography>
       );
+  }
+
+  const CadastrarUsuario = async () => {
+
+    if(name === '' || email === '' || username === '' || city === '' || password === '' || confirmPassword === ''){
+        setSeverity("warning");
+        setNotificationMessage("Preencher todos os campos!");
+        handleClick();
+        return null;
+    }
+
+    if(password !== confirmPassword){
+        setSeverity("error");
+        setNotificationMessage("Senhas não conferem!");
+        handleClick();
+        return null
+    }
+
+    const newUser = {
+        name: name,
+        user_name: username,
+        email: email,
+        city: city,
+        password: password
+    }
+    
+    const response = await createUser(newUser);
+    console.log(response.data)
+    if(response.status === 201){
+        setSeverity("success");
+        setNotificationMessage("Usuário criado com sucesso!");
+        handleClick();
+        setName('');
+        setUsername('');
+        setEmail('');
+        setCity('');
+        setPassword('');
+        setConfirmPassword('');
+        setTimeout(function(){ history.push("/entrar"); }, 3000);
+    }else{
+        setSeverity("error");
+        setNotificationMessage("Erro interno");
+        handleClick();
+    }
   }
 
   return (
@@ -50,6 +125,8 @@ const SignUp = (props) => {
                               name="name"
                               autoComplete="name"
                               autoFocus
+                              value={name}
+                              onChange={e => {setName(e.target.value)}}
                           />
                       </ Grid>
                       <Grid item lg={6} md={6} sm={12} xs={12}>
@@ -62,6 +139,8 @@ const SignUp = (props) => {
                               label="Email"
                               name="email"
                               autoComplete="email"
+                              value={email}
+                              onChange={e => {setEmail(e.target.value)}}
                           />
                       </ Grid>
                       <Grid item lg={6} md={6} sm={12} xs={12}>
@@ -74,6 +153,8 @@ const SignUp = (props) => {
                               label="Usuário"
                               name="username"
                               autoComplete="username"
+                              value={username}
+                              onChange={e => {setUsername(e.target.value)}}
                           />
                       </Grid>
                       <Grid item lg={6} md={6} sm={12} xs={12}>
@@ -86,6 +167,8 @@ const SignUp = (props) => {
                               label="Cidade"
                               name="city"
                               autoComplete="city"
+                              value={city}
+                              onChange={e => {setCity(e.target.value)}}
                           />
                       </ Grid>
                       <Grid item lg={6} md={6} sm={12} xs={12}>
@@ -99,6 +182,8 @@ const SignUp = (props) => {
                               type="password"
                               id="password"
                               autoComplete="current-password"
+                              value={password}
+                              onChange={e => {setPassword(e.target.value)}}
                           />
                       </ Grid>
                       <Grid item lg={6} md={6} sm={12} xs={12}>
@@ -112,15 +197,17 @@ const SignUp = (props) => {
                               type="password"
                               id="password"
                               autoComplete="current-password"
+                              value={confirmPassword}
+                              onChange={e => {setConfirmPassword(e.target.value)}}
                           />
                       </Grid>
                   </Grid>
                   <Button
-                      type="submit"
                       fullWidth
                       variant="contained"
                       color="primary"
                       className={classes.submit}
+                      onClick={() => CadastrarUsuario()}
                   >
                       Cadastrar
                   </Button>
@@ -137,6 +224,11 @@ const SignUp = (props) => {
           <Box>
               {Copyright}
           </Box>
+          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity={severity}>
+                    {notificationMessage}
+                </Alert>
+          </Snackbar>
       </Container>
   );
 }
